c64231bfe02ad1e59149c465629e4f6e75e52d96
DannyB <dannyb@telesofia.com>
2017-03-10 16:17:57 +0200

    improve example

--- examples/column_operations/example.rb
+++ examples/column_operations/example.rb
@@ -8,5 +8,8 @@ data.keep :date, :high, :low
 # Create a new column, :range, with the values of :high - :low
 data[:range] = data[:high] - data[:low]
 
+# Create a new column, :double, with the value of :range multiplied by 2
+data[:double] = (data[:range] * 2).round
+
 data.round :range, decimals: 2
 data.preview
